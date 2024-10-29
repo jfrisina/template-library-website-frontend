ff6d9d96b7d0275e7baf53fd121dbb9d8071f87a
Jaki Frisina <jaki.frisina@gmail.com>
2024-10-29 18:50:37 -0400

    add dungeons and dragons event

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -45,7 +45,7 @@ const Events = () => {
                             Bring your latest craft project to work on!
                             </p>
                             <p><strong>Location</strong>: Main Branch</p>  
-                                <p><strong>Date</strong>: Every Thursday</p> <p><strong>Time</strong>: 3 PM</p>
+                            <p><strong>Date</strong>: Every Thursday</p> <p><strong>Time</strong>: 3 PM</p>
                         </div>
                             <div className="usa-card__footer">
                                 <a href="#" className="usa-button">Get info</a>
@@ -55,24 +55,25 @@ const Events = () => {
                     <li className="usa-card tablet-lg:grid-col-6 widescreen:grid-col-4">
                         <div className="usa-card__container">
                             <div className="usa-card__header">
-                                <h4 className="usa-card__heading">Exdent media</h4>
+                                <h4 className="usa-card__heading">Dungeons & Dragons</h4>
                             </div>
                             <div className="usa-card__media usa-card__media--exdent">
                                 <div className="usa-card__img">
                                 <img
-                                    src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-                                    alt="A placeholder image"
+                                    src="src/assets/timothy-dykes-7-vPKU5Kqqs-unsplash.jpg"
+                                    alt="20-sided dice for the game Dungeons and Dragons"
                                 />
                                 </div>
                             </div>
                             <div className="usa-card__body">
                                 <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                                possimus similique nemo odit doloremque laudantium?
+                                Enter the world of role-playing fantasy game
                                 </p>
+                                <p><strong>Location</strong>: South Branch</p>  
+                                <p><strong>Date</strong>: Every Wednesday</p> <p><strong>Time</strong>: 4 PM</p>
                             </div>
                             <div className="usa-card__footer">
-                                <a href="#" className="usa-button">Visit Florida Keys</a>
+                                <a href="#" className="usa-button">Get Info</a>
                             </div>
                         </div>
                     </li>

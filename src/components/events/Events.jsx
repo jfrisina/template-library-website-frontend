e617fe12d432c61c62c7d4a06b4b5eb9f0026137
Jaki Frisina <jaki.frisina@gmail.com>
2024-10-29 18:44:21 -0400

    add crafternoon event

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -26,28 +26,29 @@ const Events = () => {
                                 <p><strong>Date</strong>: Every Tuesday</p> <p><strong>Time</strong>: 10 AM</p>
                             </div>
                             <div className="usa-card__footer">
-                                <a href="#" className="usa-button">Visit Florida Keys</a>
+                                <a href="#" className="usa-button">Get info</a>
                             </div>
                         </div>
                     </li>
                     <li className="usa-card tablet-lg:grid-col-6 widescreen:grid-col-4">
                         <div className="usa-card__container">
                             <div className="usa-card__header">
-                                <h4 className="usa-card__heading">Exdent media</h4>
+                                <h4 className="usa-card__heading">Crafternoon</h4>
                             </div>
                             <div className="usa-card__media usa-card__media--exdent">
                                 <div className="usa-card__img">
-                                    <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg" alt="A placeholder image"/>
+                                    <img src="src/assets/tim-arterbury-VkwRmha1_tI-unsplash.jpg" alt="A placeholder image"/>
                                 </div>
                             </div>
                         <div className="usa-card__body">
                             <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                            possimus similique nemo odit doloremque laudantium?
+                            Bring your latest craft project to work on!
                             </p>
+                            <p><strong>Location</strong>: Main Branch</p>  
+                                <p><strong>Date</strong>: Every Thursday</p> <p><strong>Time</strong>: 3 PM</p>
                         </div>
                             <div className="usa-card__footer">
-                                <a href="#" className="usa-button">Visit Florida Keys</a>
+                                <a href="#" className="usa-button">Get info</a>
                             </div>
                         </div>
                     </li>

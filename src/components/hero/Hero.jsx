469bb5ce753c37f813178b78cc3a7830cca27423
Jaki Frisina <jaki.frisina@gmail.com>
2024-11-01 21:20:46 -0400

    add overlay to hero, add overlay text, edit overlay styling

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,8 +3,13 @@ import './Hero.css'
 const Hero = () => {
     return (
         <>
-            <div className='hero'>
+            <div className="hero">
                 <img src="src/assets/images/diane-picchiottino-8v-TDZY3KBA-unsplash.jpg" alt="exterior of New York Public Library with a tree in front"/>
+                <div className="overlay"></div>
+                <div className="overlay-content">
+                    <h2>Test Header</h2>
+                    <p>Test text</p>
+                </div>
             </div>
         </>
     )

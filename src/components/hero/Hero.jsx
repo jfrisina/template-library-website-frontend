140600062d39aa91126ca0e534010fb2ea2f8208
Jaki Frisina <jaki.frisina@gmail.com>
2024-11-01 20:26:39 -0400

    add hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,9 @@ import './Hero.css'
 const Hero = () => {
     return (
         <>
+            <div className='hero'>
+                <img src="src/assets/images/diane-picchiottino-8v-TDZY3KBA-unsplash.jpg" alt="exterior of New York Public Library with a tree in front"/>
+            </div>
         </>
     )
 }
